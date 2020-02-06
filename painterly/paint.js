343089e93efb44592464da49d1130c1706d2c736
Adam Sultanov <adam.sultanov@gmail.com>
2020-02-06 17:00:35 -0500

    scale and draw

--- painterly/paint.js
+++ painterly/paint.js
@@ -7,8 +7,10 @@ const contextMenu = document.querySelector('#contextMenu');
 const canvasDiv = document.querySelector('#canvas')
 const createForm = document.querySelector('#create')
 const scaleInput = document.querySelector('#scale')
+const scaleRange = document.querySelector('#scaleRange')
 let scale = 15;
 scaleInput.value = scale;
+scaleRange.value = scale;
 let screenPicture = {}
 
 console.dir(toolbar);
@@ -20,6 +22,14 @@ console.dir(contextMenu);
 console.dir(canvasDiv);
 console.dir(createForm);
 console.dir(scaleInput);
+console.dir(scaleRange);
+
+// const state = {
+// 	picture: {w, h, [pix]},
+// 	tool: t,
+//	color: c,
+// 	...?
+// }
 
 class Picture {
   constructor(width, height, pixels) {
@@ -62,39 +72,41 @@ const createCanvas = e => {
 createForm.addEventListener('submit', createCanvas)
 
 
-
 const scaleChange = e => {
 	scale = e.target.value;
+	scaleInput.value = scale;
+	scaleRange.value = scale;
 	// would normally dispatch a thing...
 	// then have input derive value from state
 	console.log(scale);
 	drawPicture(screenPicture, canvas, scale)
 }
 scaleInput.addEventListener('change', scaleChange)
+scaleRange.addEventListener('change', scaleChange)
 
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
 // canvas.width = canvasDiv.clientWidth - 10;
-// canvas.height = canvasDiv.clientHeight - 10;q
+// canvas.height = canvasDiv.clientHeight - 10;
 canvasDiv.appendChild(canvas);
+canvas.addEventListener('click', drawPixel)
 console.log(canvas);
-
 
 
 // this component has two responsibilities: 1.displaying the picture as a grid of colored boxes and 2.communicating pointer events from this picture
 // cannot directly dispatch actions as it does not know the whole application state, only the current picture
-class PictureCanvas {
-  constructor(picture, pointerDown) {
-    this.syncState(picture);
-  }
-  syncState(picture) {
-    if (this.picture == picture) return;
-    // only redraws when given a new picture
-    this.picture = picture;
-    drawPicture(this.picture, canvas, scale);
-  }
-}
+// class PictureCanvas {
+//   constructor(picture, pointerDown) {
+//     this.syncState(picture);
+//   }
+//   syncState(picture) {
+//     if (this.picture == picture) return;
+//     // only redraws when given a new picture
+//     this.picture = picture;
+//     drawPicture(this.picture, canvas, scale);
+//   }
+// }
 
 function drawPicture(picture, canvas, scale) {
   canvas.width = picture.width * scale;
@@ -108,6 +120,14 @@ function drawPicture(picture, canvas, scale) {
       cx.fillRect(x * scale, y * scale, scale, scale);
     }
   }
+}
+
+function drawPixel(e) {
+	let coordX = Math.floor(e.offsetX/scale);
+	let coordY = Math.floor(e.offsetY/scale);
+	console.log(coordX, coordY);
+	screenPicture.pixels[screenPicture.width * coordY + coordX] = '#000000';
+	drawPicture(screenPicture, canvas, scale);
 }
 
 
